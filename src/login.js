8c0c611a945801b66b033f3e3feaccd93dc0a9e3
Llllucy <54648487+Llllucy@users.noreply.github.com>
2021-11-20 21:29:22 -0500

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import MapPosition from "./Map";
+import MapPosition from "./map";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Route, Router } from 'react-router';
